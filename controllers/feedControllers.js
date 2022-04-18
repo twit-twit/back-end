b00f3dd44bb3d36c89789505485b064da79912ff
Hojin <gbolt@naver.com>
2022-04-18 14:46:14 +0900

    진행중

--- controllers/feedControllers.js
+++ controllers/feedControllers.js
@@ -4,7 +4,8 @@ const { Op } = require("sequelize")
 
 //게시글 조회
 exports.getFeeds = async (req, res) => {
-    const { feedType, userCode } = req.query;
+    const { feedType } = req.query;
+    const userCode = 1;
 
     // include: [
     //     { model: Users, as: "users", attributes: ['userId'] }
@@ -12,18 +13,24 @@ exports.getFeeds = async (req, res) => {
     try {
         if (feedType === 'all') {
             const feedArr = await Feeds.findAll({})
-            console.log("$$$$$$", feedArr)
             let result = feedArr.sort((a, b) => b.createdAt - a.createdAt)
             res.status(200).json({ result: "SUCCESS", code: 0, result });
 
         } else if (feedType === "user") {
-            await Feeds.findAll({
-                include: [{
-                    model: Users,
-                    as: userCode,
-                    attributes: ['userId']
-                }]
-            })
+            const user = await Users.findOne({ where: { userCode: userCode } })
+            console.log("@@@@@", user)
+            const feeds = await user.getFeeds();
+            console.log("@$$$$$$$", feeds)
+
+            // await Feeds.findAll({
+            //     where: {
+            //         userCode: userCode
+            //     },
+            //     include: [{
+            //         model: Users,
+            //     }]
+            // })
+
         }
 
     } catch (err) {
@@ -38,7 +45,7 @@ exports.getFeeds = async (req, res) => {
 
 //게시글 작성
 exports.postFeeds = async (req, res) => {
-    const { userCode, title, content } = req.body;
+    const { userCode, content } = req.body;
     try {
         await Feeds.create({
             userCode,
@@ -65,10 +72,10 @@ exports.postFeeds = async (req, res) => {
 
 //게시글 삭제
 exports.deleteFeeds = async (req, res) => {
-    const { feedCode } = req.params.feedCode;
+    const { feedCode } = req.params;
 
     try {
-        await Feeds.destory({ where: { feedCode: feedCode } })
+        await Feeds.destroy({ where: { feedCode: feedCode } })
         res.status(200).json({
             result: "SUCCESS",
             code: 0,
@@ -88,29 +95,20 @@ exports.deleteFeeds = async (req, res) => {
 
 //게시글 수정
 exports.updateFeeds = async (req, res) => {
-    const { feedCode } = req.params.feedCode;
+    const { feedCode } = req.params;
     const { title, content } = req.body;
-    const { img } = req.file;
+
 
     try {
-        if (!img) {
-            await Feeds.update({ feedCode: feedCode }, { title, content })
-            res.status(200).json({
-                result: "SUCCESS",
-                code: 0,
-                message: "게시글 수정 완료!"
-            })
-        } else {
-            await Feeds.update({ feedCode: feedCode }, { title, content, img })
-            res.status(200).json({
-                result: "SUCCESS",
-                code: 0,
-                message: "게시글 수정 완료!"
-            })
-        }
+        await Feeds.update({ title, content }, { where: { feedCode: feedCode } })
+        res.status(200).json({
+            result: "SUCCESS",
+            code: 0,
+            message: "게시글 수정 완료!"
+        })
     } catch (err) {
         console.log(err)
-        res.staut(400).json({
+        res.status(400).json({
             result: "FAIL",
             code: -4,
             message: "게시글 수정 실패!"
