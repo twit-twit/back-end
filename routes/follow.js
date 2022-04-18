51b5db250453a45e5c1bf973f6b60106da596f79
zzpjw <zzpjw9710@naver.com>
2022-04-18 23:18:16 +0900

    feat(follower): follower API
    follower GET API 완성
    
    2022.04.18 clone project: twit twit
    commit by jiwon

--- routes/follow.js
+++ routes/follow.js
@@ -4,5 +4,6 @@ const followController = require("../controllers/followControllers")
 router.get('/', followController.getMyFollows);
 router.post('/', followController.postMyFollows);
 router.delete('/', followController.deleteMyFollows);
+router.get('/followers', followController.getMyFollowers);
 
 module.exports = router;
