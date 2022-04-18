89d2d603e73b1e2bd9877ea4f7862c97e56d0a2f
zzpjw <zzpjw9710@naver.com>
2022-04-18 17:36:30 +0900

    chore(follow): follow API
    팔로우 API post if문 케이스 분리
    
    2022.04.18 clone project: twit twit
    commit by jiwon

--- controllers/followControllers.js
+++ controllers/followControllers.js
@@ -1,7 +1,6 @@
 const { Follows } = require("../models")
 const { Users } = require("../models")
 
-//팔로우 조회
 exports.getMyFollows = async (req, res) => {
     /*========================================================================================================
     #swagger.tags = ['follows']
@@ -9,7 +8,6 @@ exports.getMyFollows = async (req, res) => {
     #swagger.description = '내가 팔로우한 모든 유저 목록을 조회하는 API'
     ========================================================================================================*/
     const { userCode } = req.query;
-
     try {
         const myFollows = await Follows.findAll({
             where: {
@@ -44,7 +42,6 @@ exports.getMyFollows = async (req, res) => {
     }
 }
 
-//팔로우 하기
 exports.postMyFollows = async (req, res) => {
     /*========================================================================================================
     #swagger.tags = ['follows']
@@ -52,14 +49,23 @@ exports.postMyFollows = async (req, res) => {
     #swagger.description = '팔로우 API'
     ========================================================================================================*/
     const { userCode, followUserCode } = req.body;
+    console.log(req.body)
     const findFollow = await Follows.findOne({ where: { userCode, followUserCode } })
+    console.log("변수 확인? 실제로 있을 때, 없을 때 배열인지?", typeof(findFollow), findFollow)
     /*=====================================================================================
     #swagger.responses[400] = {
         description: '비정상 값을 응답받았을 때, 아래 예제와 같은 형태로 응답받습니다.',
         schema: { "result": "FAIL", 'code': -5, 'message': "팔로우 실패", }
     }
     =====================================================================================*/
-    if (userCode===followUserCode||findFollow) {res.status(400).json({result: "FAIL", code: -5, message: "팔로우 실패"})}
+    if (userCode===followUserCode) {res.status(400).json({result: "FAIL", code: -5, message: "팔로우 실패"})}
+    /*=====================================================================================
+    #swagger.responses[400] = {
+        description: '비정상 값을 응답받았을 때, 아래 예제와 같은 형태로 응답받습니다.',
+        schema: { "result": "FAIL", 'code': -5, 'message': "팔로우 실패", }
+    }
+    =====================================================================================*/
+    if (findFollow) {res.status(400).json({result: "FAIL", code: -5, message: "팔로우 실패"})}
     try {
         await Follows.create({
             userCode,
@@ -92,7 +98,6 @@ exports.postMyFollows = async (req, res) => {
     }
 }
 
-//팔로우 삭제
 exports.deleteMyFollows = async (req, res) => {
     /*========================================================================================================
     #swagger.tags = ['follows']
@@ -101,7 +106,7 @@ exports.deleteMyFollows = async (req, res) => {
     ========================================================================================================*/
     const { userCode, followUserCode } = req.params;
     try {
-        await Follows.destroy({ 
+        await Follows.destroy({
             where: {
                 userCode,
                 followUserCode
