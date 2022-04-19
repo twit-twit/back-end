4f4a91e23918c51fad2efa3b14f5c61e9983444b
zzpjw <zzpjw9710@naver.com>
2022-04-19 16:06:06 +0900

    feat(comment): develop comment DELETE API
    Comments DELETE API 개발
    
    2022.04.19 clone project: twit twit
    commit by jiwon

--- routes/comment.js
+++ routes/comment.js
@@ -5,7 +5,7 @@ const authMiddleware = require('../middlewares/users/auth-middleware');
 router.get('/', authMiddleware, commentController.getComments);
 router.post('/', authMiddleware, commentController.postComments);
 router.put('/', authMiddleware, commentController.putComments);
-// router.delete('/', authMiddleware, commentController.deletes);
+router.delete('/', authMiddleware, commentController.deleteComments);
 // router.get('/followers', authMiddleware, commentController.gets);
 
 module.exports = router;
